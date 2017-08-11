2c47e4c4e45cacee3dc446a751f43e711ac1a94c
shepherdviolet <shepherdviolet@163.com>
2017-08-11 20:54:44 +0800

    fix>MappedByteBuffer will lock file in hotspot vm, fix it

--- src/main/java/sviolet/thistle/util/crypto/ECDSACipher.java
+++ src/main/java/sviolet/thistle/util/crypto/ECDSACipher.java
@@ -19,6 +19,7 @@
 
 package sviolet.thistle.util.crypto;
 
+import sviolet.thistle.util.common.PlatformUtils;
 import sviolet.thistle.util.file.FileUtils;
 
 import java.io.File;
@@ -78,7 +79,41 @@ public class ECDSACipher {
     }
 
     /**
+     * <p>用私钥对信息生成数字签名, 根据运行时环境选择使用NIO或IO方式</p>
+     *
+     * @param file 需要签名的文件
+     * @param privateKey 私钥
+     * @param signAlgorithm 签名逻辑: RSACipher.SIGN_ALGORITHM_RSA_MD5 / RSACipher.SIGN_ALGORITHM_RSA_SHA1
+     *
+     * @return 数字签名
+     * @throws NoSuchAlgorithmException 无效的signAlgorithm
+     * @throws InvalidKeyException 无效的私钥
+     * @throws SignatureException 签名异常
+     */
+    public static byte[] sign(File file, ECPrivateKey privateKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
+        if (PlatformUtils.PLATFORM == PlatformUtils.Platform.DALVIK){
+            //安卓API11以上使用NIO, API10以下会很慢
+            if (PlatformUtils.ANDROID_VERSION < 11){
+                return signIo(file, privateKey, signAlgorithm);
+            } else {
+                return signNio(file, privateKey, signAlgorithm);
+            }
+        }
+        //能手动回收MappedByteBuffer则使用NIO
+        if (FileUtils.isMappedByteBufferCanClean()){
+            return signNio(file, privateKey, signAlgorithm);
+        } else {
+            return signIo(file, privateKey, signAlgorithm);
+        }
+    }
+
+    /**
      * <p>用私钥对信息生成数字签名(NIO)</p>
+     *
+     * 注意:非安卓平台使用该方法前, 请使用FileUtils.isMappedByteBufferCanClean()判断MappedByteBuffer是否能被手动回收,
+     * 如果isMappedByteBufferCanClean返回false, 建议使用signIo, 否则操作后, 文件将在一段时间内无法被读写删除<br/>
+     *
+     * 注意:安卓平台API11以上使用, API10以下会很慢<br/>
      *
      * @param file 需要签名的文件
      * @param privateKey 私钥
@@ -119,6 +154,39 @@ public class ECDSACipher {
     }
 
     /**
+     * <p>用私钥对信息生成数字签名(IO)</p>
+     *
+     * @param file 需要签名的文件
+     * @param privateKey 私钥
+     * @param signAlgorithm 签名逻辑: ECDSACipher.SIGN_ALGORITHM_ECDSA_SHA256
+     *
+     * @return 数字签名
+     * @throws NoSuchAlgorithmException 无效的signAlgorithm
+     * @throws InvalidKeyException 无效的私钥
+     * @throws SignatureException 签名异常
+     */
+    public static byte[] signIo(File file, ECPrivateKey privateKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            Signature signature = generateECDSASignatureInstance(privateKey, signAlgorithm);
+            byte[] buff = new byte[1024];
+            int size;
+            while((size = inputStream.read(buff)) != -1){
+                signature.update(buff, 0, size);
+            }
+            return signature.sign();
+        } finally {
+            if (inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+
+    /**
      * <p>用公钥验证数字签名</p>
      *  
      * @param data 被签名的数据
@@ -140,7 +208,42 @@ public class ECDSACipher {
     }
 
     /**
+     * <p>用公钥验证数字签名, 根据运行时环境选择使用NIO或IO方式</p>
+     *
+     * @param file 被签名的文件
+     * @param sign 数字签名
+     * @param publicKey 公钥
+     * @param signAlgorithm 签名逻辑: RSACipher.SIGN_ALGORITHM_RSA_MD5 / RSACipher.SIGN_ALGORITHM_RSA_SHA1
+     *
+     * @return true:数字签名有效
+     * @throws NoSuchAlgorithmException 无效的signAlgorithm
+     * @throws InvalidKeyException 无效的私钥
+     * @throws SignatureException 签名异常
+     */
+    public static boolean verify(File file, byte[] sign, ECPublicKey publicKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
+        if (PlatformUtils.PLATFORM == PlatformUtils.Platform.DALVIK){
+            //安卓API11以上使用NIO, API10以下会很慢
+            if (PlatformUtils.ANDROID_VERSION < 11){
+                return verifyIo(file, sign, publicKey, signAlgorithm);
+            } else {
+                return verifyNio(file, sign, publicKey, signAlgorithm);
+            }
+        }
+        //能手动回收MappedByteBuffer则使用NIO
+        if (FileUtils.isMappedByteBufferCanClean()){
+            return verifyNio(file, sign, publicKey, signAlgorithm);
+        } else {
+            return verifyIo(file, sign, publicKey, signAlgorithm);
+        }
+    }
+
+    /**
      * <p>用公钥验证数字签名(NIO)</p>
+     *
+     * 注意:非安卓平台使用该方法前, 请使用FileUtils.isMappedByteBufferCanClean()判断MappedByteBuffer是否能被手动回收,
+     * 如果isMappedByteBufferCanClean返回false, 建议使用signIo, 否则操作后, 文件将在一段时间内无法被读写删除<br/>
+     *
+     * 注意:安卓平台API11以上使用, API10以下会很慢<br/>
      *
      * @param file 被签名的文件
      * @param sign 数字签名
@@ -180,6 +283,42 @@ public class ECDSACipher {
             }
             //尝试将MappedByteBuffer回收, 解决后续文件无法被读写删除的问题
             FileUtils.cleanMappedByteBuffer(byteBuffer);
+        }
+    }
+
+    /**
+     * <p>用公钥验证数字签名(IO)</p>
+     *
+     * @param file 被签名的文件
+     * @param sign 数字签名
+     * @param publicKey 公钥
+     * @param signAlgorithm 签名逻辑: ECDSACipher.SIGN_ALGORITHM_ECDSA_SHA256
+     *
+     * @return true:数字签名有效
+     * @throws NoSuchAlgorithmException 无效的signAlgorithm
+     * @throws InvalidKeyException 无效的私钥
+     * @throws SignatureException 签名异常
+     *
+     */
+    public static boolean verifyIo(File file, byte[] sign, ECPublicKey publicKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            Signature signature = Signature.getInstance(signAlgorithm);
+            signature.initVerify(publicKey);
+            byte[] buff = new byte[1024];
+            int size;
+            while((size = inputStream.read(buff)) != -1){
+                signature.update(buff, 0, size);
+            }
+            return signature.verify(sign);
+        } finally {
+            if (inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException ignored) {
+                }
+            }
         }
     }
 
