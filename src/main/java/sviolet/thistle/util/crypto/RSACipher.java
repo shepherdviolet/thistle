976e15fc45f14173c6267e47faf7b82aa401b6e5
shepherdviolet <shepherdviolet@163.com>
2017-08-11 20:53:12 +0800

    fix>RSACipher解决MappedByteBuffer锁住文件的问题

--- src/main/java/sviolet/thistle/util/crypto/RSACipher.java
+++ src/main/java/sviolet/thistle/util/crypto/RSACipher.java
@@ -19,11 +19,13 @@
 
 package sviolet.thistle.util.crypto;
 
+import sviolet.thistle.util.common.PlatformUtils;
+import sviolet.thistle.util.file.FileUtils;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.security.InvalidKeyException;
@@ -60,7 +62,19 @@ public class RSACipher {
     public static final String CRYPTO_TRANSFORMATION_RSA_ECB_NOPADDING = "RSA/ECB/NoPadding";
     public static final String CRYPTO_TRANSFORMATION_RSA_NONE_NOPADDING = "RSA/None/NoPadding";
     public static final String CRYPTO_TRANSFORMATION_RSA = "RSA";
-	  
+
+    /**
+     * @param privateKey 私钥
+     * @param signAlgorithm 签名逻辑: RSACipher.SIGN_ALGORITHM_RSA_MD5 / RSACipher.SIGN_ALGORITHM_RSA_SHA1
+     * @throws NoSuchAlgorithmException 无效的signAlgorithm
+     * @throws InvalidKeyException 无效的私钥
+     */
+    public static Signature generateSignatureInstance(RSAPrivateKey privateKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException {
+        Signature signature = Signature.getInstance(signAlgorithm);
+        signature.initSign(privateKey);
+        return signature;
+    }
+
     /**
      * 用私钥对信息生成数字签名<p>
      *  
@@ -80,45 +94,7 @@ public class RSACipher {
     }
 
     /**
-     * <p>用私钥对信息生成数字签名(NIO)</p>
-     *
-     * <p>ByteBuffer示例:</p>
-     *
-     * <pre>{@code
-     *         FileInputStream inputStream = new FileInputStream(file);
-     *         FileChannel channel = inputStream.getChannel();
-     *         MappedByteBuffer byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
-     * }</pre>
-     *
-     * @param data 需要签名的数据
-     * @param privateKey 私钥
-     * @param signAlgorithm 签名逻辑: RSACipher.SIGN_ALGORITHM_RSA_MD5 / RSACipher.SIGN_ALGORITHM_RSA_SHA1
-     *
-     * @return 数字签名
-     * @throws NoSuchAlgorithmException 无效的signAlgorithm
-     * @throws InvalidKeyException 无效的私钥
-     * @throws SignatureException 签名异常
-     */
-    public static byte[] sign(ByteBuffer data, RSAPrivateKey privateKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException{
-        Signature signature = generateSignatureInstance(privateKey, signAlgorithm);
-        signature.update(data);
-        return signature.sign();
-    }
-
-    /**
-     * @param privateKey 私钥
-     * @param signAlgorithm 签名逻辑: RSACipher.SIGN_ALGORITHM_RSA_MD5 / RSACipher.SIGN_ALGORITHM_RSA_SHA1
-     * @throws NoSuchAlgorithmException 无效的signAlgorithm
-     * @throws InvalidKeyException 无效的私钥
-     */
-    public static Signature generateSignatureInstance(RSAPrivateKey privateKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException {
-        Signature signature = Signature.getInstance(signAlgorithm);
-        signature.initSign(privateKey);
-        return signature;
-    }
-
-    /**
-     * <p>用私钥对信息生成数字签名(NIO)</p>
+     * <p>用私钥对信息生成数字签名, 根据运行时环境选择使用NIO或IO方式</p>
      *
      * @param file 需要签名的文件
      * @param privateKey 私钥
@@ -130,12 +106,91 @@ public class RSACipher {
      * @throws SignatureException 签名异常
      */
     public static byte[] sign(File file, RSAPrivateKey privateKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
+        if (PlatformUtils.PLATFORM == PlatformUtils.Platform.DALVIK){
+            //安卓API11以上使用NIO, API10以下会很慢
+            if (PlatformUtils.ANDROID_VERSION < 11){
+                return signIo(file, privateKey, signAlgorithm);
+            } else {
+                return signNio(file, privateKey, signAlgorithm);
+            }
+        }
+        //能手动回收MappedByteBuffer则使用NIO
+        if (FileUtils.isMappedByteBufferCanClean()){
+            return signNio(file, privateKey, signAlgorithm);
+        } else {
+            return signIo(file, privateKey, signAlgorithm);
+        }
+    }
+
+    /**
+     * <p>用私钥对信息生成数字签名(NIO)</p>
+     *
+     * 注意:非安卓平台使用该方法前, 请使用FileUtils.isMappedByteBufferCanClean()判断MappedByteBuffer是否能被手动回收,
+     * 如果isMappedByteBufferCanClean返回false, 建议使用signIo, 否则操作后, 文件将在一段时间内无法被读写删除<br/>
+     *
+     * 注意:安卓平台API11以上使用, API10以下会很慢<br/>
+     *
+     * @param file 需要签名的文件
+     * @param privateKey 私钥
+     * @param signAlgorithm 签名逻辑: RSACipher.SIGN_ALGORITHM_RSA_MD5 / RSACipher.SIGN_ALGORITHM_RSA_SHA1
+     *
+     * @return 数字签名
+     * @throws NoSuchAlgorithmException 无效的signAlgorithm
+     * @throws InvalidKeyException 无效的私钥
+     * @throws SignatureException 签名异常
+     */
+    public static byte[] signNio(File file, RSAPrivateKey privateKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
+        FileInputStream inputStream = null;
+        FileChannel channel = null;
+        MappedByteBuffer byteBuffer = null;
+        try {
+            inputStream = new FileInputStream(file);
+            channel = inputStream.getChannel();
+            byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+            Signature signature = generateSignatureInstance(privateKey, signAlgorithm);
+            signature.update(byteBuffer);
+            return signature.sign();
+        } finally {
+            if (inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException ignored) {
+                }
+            }
+            if (channel != null){
+                try {
+                    channel.close();
+                } catch (IOException ignored) {
+                }
+            }
+            //尝试将MappedByteBuffer回收, 解决后续文件无法被读写删除的问题
+            FileUtils.cleanMappedByteBuffer(byteBuffer);
+        }
+    }
+
+    /**
+     * <p>用私钥对信息生成数字签名(IO)</p>
+     *
+     * @param file 需要签名的文件
+     * @param privateKey 私钥
+     * @param signAlgorithm 签名逻辑: RSACipher.SIGN_ALGORITHM_RSA_MD5 / RSACipher.SIGN_ALGORITHM_RSA_SHA1
+     *
+     * @return 数字签名
+     * @throws NoSuchAlgorithmException 无效的signAlgorithm
+     * @throws InvalidKeyException 无效的私钥
+     * @throws SignatureException 签名异常
+     */
+    public static byte[] signIo(File file, RSAPrivateKey privateKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
         FileInputStream inputStream = null;
         try {
             inputStream = new FileInputStream(file);
-            FileChannel channel = inputStream.getChannel();
-            MappedByteBuffer byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
-            return sign(byteBuffer, privateKey, signAlgorithm);
+            Signature signature = generateSignatureInstance(privateKey, signAlgorithm);
+            byte[] buff = new byte[1024];
+            int size;
+            while((size = inputStream.read(buff)) != -1){
+                signature.update(buff, 0, size);
+            }
+            return signature.sign();
         } finally {
             if (inputStream != null){
                 try {
@@ -168,17 +223,9 @@ public class RSACipher {
     }
 
     /**
-     * <p>用公钥验证数字签名(NIO)</p>
+     * <p>用公钥验证数字签名, 根据运行时环境选择使用NIO或IO方式</p>
      *
-     * <p>ByteBuffer示例:</p>
-     *
-     * <pre>{@code
-     *         FileInputStream inputStream = new FileInputStream(file);
-     *         FileChannel channel = inputStream.getChannel();
-     *         MappedByteBuffer byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
-     * }</pre>
-     *
-     * @param data 被签名的数据
+     * @param file 被签名的文件
      * @param sign 数字签名
      * @param publicKey 公钥
      * @param signAlgorithm 签名逻辑: RSACipher.SIGN_ALGORITHM_RSA_MD5 / RSACipher.SIGN_ALGORITHM_RSA_SHA1
@@ -187,17 +234,31 @@ public class RSACipher {
      * @throws NoSuchAlgorithmException 无效的signAlgorithm
      * @throws InvalidKeyException 无效的私钥
      * @throws SignatureException 签名异常
-     *
      */
-    public static boolean verify(ByteBuffer data, byte[] sign, RSAPublicKey publicKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException{
-        Signature signature = Signature.getInstance(signAlgorithm);
-        signature.initVerify(publicKey);
-        signature.update(data);
-        return signature.verify(sign);
+    public static boolean verify(File file, byte[] sign, RSAPublicKey publicKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
+        if (PlatformUtils.PLATFORM == PlatformUtils.Platform.DALVIK){
+            //安卓API11以上使用NIO, API10以下会很慢
+            if (PlatformUtils.ANDROID_VERSION < 11){
+                return verifyIo(file, sign, publicKey, signAlgorithm);
+            } else {
+                return verifyNio(file, sign, publicKey, signAlgorithm);
+            }
+        }
+        //能手动回收MappedByteBuffer则使用NIO
+        if (FileUtils.isMappedByteBufferCanClean()){
+            return verifyNio(file, sign, publicKey, signAlgorithm);
+        } else {
+            return verifyIo(file, sign, publicKey, signAlgorithm);
+        }
     }
 
     /**
      * <p>用公钥验证数字签名(NIO)</p>
+     *
+     * 注意:非安卓平台使用该方法前, 请使用FileUtils.isMappedByteBufferCanClean()判断MappedByteBuffer是否能被手动回收,
+     * 如果isMappedByteBufferCanClean返回false, 建议使用verifyIo, 否则操作后, 文件将在一段时间内无法被读写删除<br/>
+     *
+     * 注意:安卓平台API11以上使用, API10以下会很慢<br/>
      *
      * @param file 被签名的文件
      * @param sign 数字签名
@@ -210,13 +271,62 @@ public class RSACipher {
      * @throws SignatureException 签名异常
      *
      */
-    public static boolean verify(File file, byte[] sign, RSAPublicKey publicKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
+    public static boolean verifyNio(File file, byte[] sign, RSAPublicKey publicKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
+        FileInputStream inputStream = null;
+        FileChannel channel = null;
+        MappedByteBuffer byteBuffer = null;
+        try {
+            inputStream = new FileInputStream(file);
+            channel = inputStream.getChannel();
+            byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+            Signature signature = Signature.getInstance(signAlgorithm);
+            signature.initVerify(publicKey);
+            signature.update(byteBuffer);
+            return signature.verify(sign);
+        } finally {
+            if (inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException ignored) {
+                }
+            }
+            if (channel != null){
+                try {
+                    channel.close();
+                } catch (IOException ignored) {
+                }
+            }
+            //尝试将MappedByteBuffer回收, 解决后续文件无法被读写删除的问题
+            FileUtils.cleanMappedByteBuffer(byteBuffer);
+        }
+    }
+
+    /**
+     * <p>用公钥验证数字签名(IO)</p>
+     *
+     * @param file 被签名的文件
+     * @param sign 数字签名
+     * @param publicKey 公钥
+     * @param signAlgorithm 签名逻辑: RSACipher.SIGN_ALGORITHM_RSA_MD5 / RSACipher.SIGN_ALGORITHM_RSA_SHA1
+     *
+     * @return true:数字签名有效
+     * @throws NoSuchAlgorithmException 无效的signAlgorithm
+     * @throws InvalidKeyException 无效的私钥
+     * @throws SignatureException 签名异常
+     *
+     */
+    public static boolean verifyIo(File file, byte[] sign, RSAPublicKey publicKey, String signAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, IOException {
         FileInputStream inputStream = null;
         try {
             inputStream = new FileInputStream(file);
-            FileChannel channel = inputStream.getChannel();
-            MappedByteBuffer byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
-            return verify(byteBuffer, sign, publicKey, signAlgorithm);
+            Signature signature = Signature.getInstance(signAlgorithm);
+            signature.initVerify(publicKey);
+            byte[] buff = new byte[1024];
+            int size;
+            while((size = inputStream.read(buff)) != -1){
+                signature.update(buff, 0, size);
+            }
+            return signature.verify(sign);
         } finally {
             if (inputStream != null){
                 try {
