42e2cb9c17831c4f5eae776d83981b7413442591
shepherdviolet <shepherdviolet@163.com>
2017-11-03 20:17:11 +0800

    feat>增加大文件加解密方法

--- src/main/java/sviolet/thistle/util/crypto/AESCipher.java
+++ src/main/java/sviolet/thistle/util/crypto/AESCipher.java
@@ -19,15 +19,14 @@
 
 package sviolet.thistle.util.crypto;
 
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
 
 /**
  * 
@@ -61,8 +60,48 @@ public class AESCipher{
 		Cipher cipher = Cipher.getInstance(cryptoTransformation);// 创建密码器
 		cipher.init(Cipher.ENCRYPT_MODE, keySpec);// 初始化
 		return cipher.doFinal(data);
-	} 
-	
+	}
+
+	/**
+	 * 加密(大文件), 注意, 输入输出流会被关闭
+	 *
+	 * @param in 待加密数据流
+     * @param out 加密后数据流
+	 * @param key 秘钥(通常只支持128)
+	 * @param cryptoTransformation 加密算法/填充算法
+	 *
+	 * @throws NoSuchAlgorithmException 加密算法无效
+	 * @throws NoSuchPaddingException 填充算法无效
+	 * @throws InvalidKeyException 秘钥无效
+	 * @throws IllegalBlockSizeException 块大小无效
+	 * @throws BadPaddingException 填充错误(密码错?)
+	 * @throws InvalidAlgorithmParameterException 算法参数无效
+	 */
+	public static void encrypt(InputStream in, OutputStream out, byte[] key, String cryptoTransformation) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException {
+		if (in == null) {
+		    throw new NullPointerException("in is null");
+        }
+        if (out == null) {
+            throw new NullPointerException("out is null");
+        }
+
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(key, AESKeyGenerator.AES_KEY_ALGORITHM);
+            Cipher cipher = Cipher.getInstance(cryptoTransformation);// 创建密码器
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);// 初始化
+
+            in = new CipherInputStream(in, cipher);
+            byte[] buff = new byte[1024 * 32];//32K buff
+            int length;
+            while ((length = in.read(buff)) >= 0) {
+                out.write(buff, 0, length);
+            }
+        } finally {
+            in.close();
+            out.close();
+        }
+    }
+
 	/**
 	 * 解密
 	 *
@@ -83,4 +122,45 @@ public class AESCipher{
 		cipher.init(Cipher.DECRYPT_MODE, keySpec);// 初始化
 		return cipher.doFinal(data);
 	}
+
+    /**
+     * 解密(大文件), 注意, 输入输出流会被关闭
+     *
+     * @param in 待解密数据流
+     * @param out 解密后数据流
+     * @param key 秘钥(通常只支持128)
+     * @param cryptoTransformation 加密算法/填充算法
+     *
+     * @throws NoSuchAlgorithmException 加密算法无效
+     * @throws NoSuchPaddingException 填充算法无效
+     * @throws InvalidKeyException 秘钥无效
+     * @throws IllegalBlockSizeException 块大小无效
+     * @throws BadPaddingException 填充错误(密码错?)
+     * @throws InvalidAlgorithmParameterException 算法参数无效
+     */
+    public static void decrypt(InputStream in, OutputStream out, byte[] key, String cryptoTransformation) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException {
+        if (in == null) {
+            throw new NullPointerException("in is null");
+        }
+        if (out == null) {
+            throw new NullPointerException("out is null");
+        }
+
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(key, AESKeyGenerator.AES_KEY_ALGORITHM);
+            Cipher cipher = Cipher.getInstance(cryptoTransformation);// 创建密码器
+            cipher.init(Cipher.DECRYPT_MODE, keySpec);// 初始化
+
+            out = new CipherOutputStream(out, cipher);
+            byte[] buff = new byte[1024 * 32];//32K buff
+            int length;
+            while ((length = in.read(buff)) >= 0) {
+                out.write(buff, 0, length);
+            }
+        } finally {
+            in.close();
+            out.close();
+        }
+    }
+
 }
---
+++ src/test/java/sviolet/thistle/util/crypto/AESCipherTest.java
@@ -0,0 +1,45 @@
+package sviolet.thistle.util.crypto;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class AESCipherTest {
+
+    @Test
+    public void streamEncryptDecrypt() throws IOException, NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+
+        String data = "我是中国人呢";
+        byte[] dataBytes = data.getBytes("UTF-8");
+        byte[] key = AESKeyGenerator.generateShaKey128("key".getBytes());
+
+//        System.out.println(ByteUtils.bytesToHex(dataBytes));
+
+        ByteArrayInputStream in = new ByteArrayInputStream(data.getBytes("UTF-8"));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        AESCipher.encrypt(in, out, key, AESCipher.CRYPTO_TRANSFORMATION_AES_ECB_PKCS5PADDING);
+        byte[] encrypted = out.toByteArray();
+
+//        System.out.println(ByteUtils.bytesToHex(encrypted));
+
+        in = new ByteArrayInputStream(encrypted);
+        out = new ByteArrayOutputStream();
+        AESCipher.decrypt(in, out, key, AESCipher.CRYPTO_TRANSFORMATION_AES_ECB_PKCS5PADDING);
+        byte[] decrypted = out.toByteArray();
+
+//        System.out.println(ByteUtils.bytesToHex(decrypted));
+
+        Assert.assertEquals(data, new String(decrypted, "UTF-8"));
+
+    }
+
+}
