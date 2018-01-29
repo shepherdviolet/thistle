347a4f97372a55dc56392ebff9a772cfc035cb14
shepherdviolet <shepherdviolet@163.com>
2018-01-29 21:31:42 +0800

    添加注释

--- src/main/java/sviolet/thistle/util/reflect/ReflectGetter.java
+++ src/main/java/sviolet/thistle/util/reflect/ReflectGetter.java
@@ -31,15 +31,44 @@ import java.util.List;
  */
 public class ReflectGetter {
 
+
+    /**
+     * <p>解析键路径</p>
+     *
+     * <p>示例:body.infoList[3].name</p>
+     *
+     * <p>
+     * 转移符: <br>
+     * // -> / <br>
+     * /. -> . <br>
+     * /[ -> [ <br>
+     * /] -> ] <br>
+     * </p>
+     *
+     * @param keyPath 键路径(字符串)
+     * @return 键路径
+     * @throws ReflectGetter.IllegalKeyPathException 无效的键路径
+     */
     public static KeyPath parseKeyPath(String keyPath) throws ReflectGetter.IllegalKeyPathException {
+        //split
         List<String> elements = splitPath(keyPath);
+        //result
         KeyPath firstElement = null;
+        //previous
         KeyPath previousElement = null;
+        //current
         KeyPath currentElement;
         for (String element : elements) {
+            //record origin for log
             String elementOrigin = element;
+            //new key path
             currentElement = new KeyPath();
+
             if (!CheckUtils.isEmpty(element)){
+
+                /*
+                    element not null
+                 */
 
                 /*
                     0: init stat
@@ -55,6 +84,10 @@ public class ReflectGetter {
 
                     char current = element.charAt(i);
                     if (escape) {
+
+                        /*
+                            previous is escape /
+                         */
 
                         switch (current) {
                             case '/':
@@ -74,6 +107,10 @@ public class ReflectGetter {
                         escape = false;
 
                     } else {
+
+                        /*
+                            normal char
+                         */
 
                         //chars after "key[index1][index2]"
                         if (step > 4){
@@ -127,13 +164,21 @@ public class ReflectGetter {
 
                 }
 
+                // no [] element
                 if (step == 0){
                     currentElement.key = element;
                 }
 
             } else {
+
+                /*
+                    element is null
+                 */
+
                 currentElement.key = "";
+
             }
+
             if (firstElement == null){
                 firstElement = currentElement;
             }
@@ -145,17 +190,31 @@ public class ReflectGetter {
         return firstElement;
     }
 
+    /**
+     * split keyPath by .
+     * @param keyPath keyPath
+     * @return List<String>
+     * @throws ReflectGetter.IllegalKeyPathException exception
+     */
     private static List<String> splitPath(String keyPath) throws ReflectGetter.IllegalKeyPathException{
+        //record origin for log
         String keyPathOrigin = keyPath;
+        //result
         List<String> list = new ArrayList<>();
         if (keyPath == null){
             return list;
         }
+
         boolean escape = false;
         int start = 0;
         for (int i = 0 ; i < keyPath.length() ; i++){
             char current = keyPath.charAt(i);
             if (escape) {
+
+                /*
+                    previous char is escape /
+                 */
+
                 switch (current) {
                     case '.':
                         if (i < 2){
@@ -173,8 +232,15 @@ public class ReflectGetter {
                     default:
                         throw new ReflectGetter.IllegalKeyPathException("Illegal keyPath, unexpected \"/" + current + "\", index:" + i + ", keyPath:" + keyPathOrigin);
                 }
+
                 escape = false;
+
             } else {
+
+                /*
+                    normal char
+                 */
+
                 switch (current) {
                     case '/':
                         escape = true;
@@ -188,13 +254,17 @@ public class ReflectGetter {
                 }
             }
         }
+
+        //remain
         if (start <= keyPath.length()){
             list.add(keyPath.substring(start, keyPath.length()));
         }
         return list;
     }
 
-
+    /**
+     * 键路径实例
+     */
     public final static class KeyPath {
 
         private String key;
@@ -217,12 +287,18 @@ public class ReflectGetter {
 
     //Exceptions///////////////////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * 类型不匹配异常
+     */
     public static class TypeNotMatchException extends Exception{
         public TypeNotMatchException(String message) {
             super(message);
         }
     }
 
+    /**
+     * 非法键路径异常
+     */
     public static class IllegalKeyPathException extends Exception {
         public IllegalKeyPathException(String message) {
             super(message);
