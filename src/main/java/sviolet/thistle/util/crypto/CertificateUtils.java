c0f898ba3feb93935536561100e3f807df76b6df
shepherdviolet <shepherdviolet@163.com>
2018-02-11 21:07:51 +0800

    修改说明

--- src/main/java/sviolet/thistle/util/crypto/CertificateUtils.java
+++ src/main/java/sviolet/thistle/util/crypto/CertificateUtils.java
@@ -48,7 +48,7 @@ public class CertificateUtils {
     public static final String SIGN_ALGORITHM_RSA_SHA1 = "SHA1withRSA";
     public static final String SIGN_ALGORITHM_RSA_SHA256 = "SHA256withRSA";
 
-    //parse//////////////////////////////////////////////////////////////////////////////////////////////////
+    //Parse//////////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
      * <p>解析X509格式的证书, 返回Certificate对象, 可用来获取证书公钥实例等</p>
@@ -74,7 +74,7 @@ public class CertificateUtils {
         }
     }
 
-    //generate////////////////////////////////////////////////////////////////////////////////////////////////
+    //Generate by Bouncy Castle///////////////////////////////////////////////////////////////////////////////////
 
     /**
      * 生成根证书(自签名证书)
@@ -192,48 +192,26 @@ public class CertificateUtils {
     //Bouncy Castle///////////////////////////////////////////////////////////////////////////////////////////
 
     /**
-     *  <p><pre>
-     *  证书版本:cert.getVersion()
-     *  序列号:cert.getSerialNumber().getValue().toString(16)
-     *  算法标识:cert.getSignatureAlgorithm().getObjectId().getId()
-     *  签发者:cert.getIssuer()
-     *  开始时间:cert.getStartDate().getTime()
-     *  结束时间:cert.getEndDate().getTime()
-     *  主体名:cert.getSubject()
-     *  签名值:cert.getSignature().getBytes()
-     *
-     *  主体公钥:SubjectPublicKeyInfo publicInfo = cert.getSubjectPublicKeyInfo();
-     *  标识符:publicInfo.getAlgorithmId().getObjectId().getId()
-     *  公钥值:publicInfo.getPublicKeyData().getBytes()
-     *  </pre></p>
-     *
-     *  <p>
-     *  标识符:<Br>
-     *  <pre>
-     *  rsaEncryption	RSA算法标识	1.2.840.113549.1.1.1
-     *  sha1withRSAEncryption	SHA1的RSA签名	1.2.840.113549.1.1.5
-     *  ECC	ECC算法标识	1.2.840.10045.2.1
-     *  SM2	SM2算法标识	1.2.156.10197.1.301
-     *  SM3WithSM2	SM3的SM2签名	1.2.156.10197.1.501
-     *  sha1withSM2	SHA1的SM2签名	1.2.156.10197.1.502
-     *  sha256withSM2	SHA256的SM2签名	1.2.156.10197.1.503
-     *  sm3withRSAEncryption	SM3的RSA签名	1.2.156.10197.1.504
-     *  commonName	主体名	2.5.4.3
-     *  emailAddress	邮箱	1.2.840.113549.1.9.1
-     *  cRLDistributionPoints	CRL分发点	2.5.29.31
-     *  extKeyUsage	扩展密钥用法	2.5.29.37
-     *  subjectAltName	使用者备用名称	2.5.29.17
-     *  CP	证书策略	2.5.29.32
-     *  clientAuth	客户端认证	1.3.6.1.5.5.7.3.2
-     *  </pre></p>
+     * <p>解析ASN.1编码的X509证书数据</p>
      *
      * @param certBase64 X509证书数据, ASN.1编码, Base64编码
      */
-//    public static X509CertificateStructure parseX509ToStructure (String certBase64) throws IOException {
+//    public static X509CertificateStructure parseX509ToStructure(String certBase64) throws IOException {
 //        return parseX509ToStructure(Base64Utils.decode(certBase64));
 //    }
 
     /**
+     * <p>解析ASN.1编码的X509证书数据</p>
+     *
+     * @param certData X509证书数据, ASN.1编码, 非Base64编码
+     */
+//    public static X509CertificateStructure parseX509ToStructure(byte[] certData) throws IOException {
+//        return parseX509ToStructure(new ByteArrayInputStream(certData));
+//    }
+
+    /**
+     * <p>解析ASN.1编码的X509证书数据</p>
+     *
      * <p><pre>
      *  证书版本:cert.getVersion()
      *  序列号:cert.getSerialNumber().getValue().toString(16)
@@ -269,13 +247,21 @@ public class CertificateUtils {
      *  clientAuth	客户端认证	1.3.6.1.5.5.7.3.2
      *  </pre></p>
      *
-     * @param certData X509证书数据, ASN.1编码, 非Base64编码
+     * @param inputStream X509证书输入流, ASN.1编码, 非Base64编码
      */
-//    public static X509CertificateStructure parseX509ToStructure (byte[] certData) throws IOException {
-//        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(certData);
-//        ASN1InputStream asn1InputStream = new ASN1InputStream(byteArrayInputStream);
-//        ASN1Sequence seq = (ASN1Sequence) asn1InputStream.readObject();
-//        return new X509CertificateStructure(seq);
+//    public static X509CertificateStructure parseX509ToStructure(InputStream inputStream) throws IOException {
+//        try {
+//            ASN1InputStream asn1InputStream = new ASN1InputStream(inputStream);
+//            ASN1Sequence seq = (ASN1Sequence) asn1InputStream.readObject();
+//            return new X509CertificateStructure(seq);
+//        }finally {
+//            if (inputStream != null) {
+//                try {
+//                    inputStream.close();
+//                } catch (Throwable ignore){
+//                }
+//            }
+//        }
 //    }
 
 }
