d6841f477a8687639b58de36240dcfae6d7afeb6
shepherdviolet <shepherdviolet@163.com>
2018-12-07 21:05:45 +0800

    新增pfx证书读取工具

--- thistle-crypto-plus/src/main/java/sviolet/thistle/util/crypto/AdvancedPKCS12KeyStoreUtils.java
+++ thistle-crypto-plus/src/main/java/sviolet/thistle/util/crypto/AdvancedPKCS12KeyStoreUtils.java
@@ -31,15 +31,16 @@ import org.bouncycastle.pkcs.bc.BcPKCS12MacCalculatorBuilder;
 import org.bouncycastle.pkcs.bc.BcPKCS12PBEOutputEncryptorBuilder;
 import org.bouncycastle.pkcs.jcajce.JcaPKCS12SafeBagBuilder;
 import sviolet.thistle.util.common.CloseableUtils;
+import sviolet.thistle.util.crypto.base.BouncyCastleProviderUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.io.*;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * <p>PKCS12密钥文件工具(p12/pfx), BouncyCastle版本</p>
@@ -58,8 +59,14 @@ import java.security.cert.X509Certificate;
  */
 public class AdvancedPKCS12KeyStoreUtils extends PKCS12KeyStoreUtils {
 
+    private static final String ALGORITHM = "PKCS12";
+
+    static {
+        BouncyCastleProviderUtils.installProvider();
+    }
+
     /**
-     * 将证书和私钥保存到p12/pfx文件中, 支持国密
+     * 将证书和私钥保存到p12/pfx文件中, BouncyCastle实现, 支持SM2
      *
      * @param keyStorePath keyStore的文件路径
      * @param keystorePassword keyStore的密码
@@ -80,7 +87,7 @@ public class AdvancedPKCS12KeyStoreUtils extends PKCS12KeyStoreUtils {
     }
 
     /**
-     * 将证书和私钥保存到p12/pfx文件中, 支持国密
+     * 将证书和私钥保存到p12/pfx文件中, BouncyCastle实现, 支持SM2
      *
      * @param outputStream keyStore的输出流, 完成后会关闭
      * @param keystorePassword keyStore的密码
@@ -99,7 +106,7 @@ public class AdvancedPKCS12KeyStoreUtils extends PKCS12KeyStoreUtils {
     }
 
     /**
-     * 将证书和私钥转换为PKCS12格式的数据(p12/pfx的数据), 支持国密
+     * 将证书和私钥转换为PKCS12格式的数据(p12/pfx的数据), BouncyCastle实现, 支持SM2
      *
      * @param keystorePassword keyStore的密码
      * @param alias 证书和私钥的别名
@@ -119,9 +126,10 @@ public class AdvancedPKCS12KeyStoreUtils extends PKCS12KeyStoreUtils {
         PKCS12SafeBag[] bags = new PKCS12SafeBag[certificateChain.length];
         for (int i = 0; i < certificateChain.length; i++) {
             PKCS12SafeBagBuilder bagBuilder = new JcaPKCS12SafeBagBuilder(certificateChain[i]);
-            bagBuilder.addBagAttribute(
-                    PKCSObjectIdentifiers.pkcs_9_at_friendlyName,
-                    new DERBMPString(alias));
+            //证书设置了alias反而会导致读取的时候读取不到全部的证书, 不知为何
+//            bagBuilder.addBagAttribute(
+//                    PKCSObjectIdentifiers.pkcs_9_at_friendlyName,
+//                    new DERBMPString(alias));
             if (i == 0) {
                 bagBuilder.addBagAttribute(
                         PKCSObjectIdentifiers.pkcs_9_at_localKeyId,
@@ -154,6 +162,162 @@ public class AdvancedPKCS12KeyStoreUtils extends PKCS12KeyStoreUtils {
         //build pfx
         return pfxPduBuilder.build(new BcPKCS12MacCalculatorBuilder(), passwordChars)
                 .getEncoded(ASN1Encoding.DER);
+    }
+
+    /**
+     * 从p12/pfx文件中遍历alias, BouncyCastle实现, 支持SM2
+     *
+     * <pre>{@code
+     *      Enumeration<String> aliases = PKCS12KeyStoreUtils.loadAliases(
+     *          "ca-cert.p12",
+     *          "000000"
+     *          );
+     * }</pre>
+     *
+     * @param keyStorePath keyStore路径
+     * @param keystorePassword keyStore密码
+     */
+    public static Enumeration<String> loadAliasesAdvanced(String keyStorePath, String keystorePassword) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException, NoSuchProviderException {
+        File keyStoreFile = new File(keyStorePath);
+        if (!keyStoreFile.exists()){
+            throw new IOException("Can not find keyStore file, path:" + keyStoreFile.getAbsolutePath());
+        }
+        return loadAliasesAdvanced(new FileInputStream(keyStoreFile), keystorePassword);
+    }
+
+    /**
+     * 从p12/pfx文件中遍历alias, BouncyCastle实现, 支持SM2
+     *
+     * <pre>{@code
+     *      Enumeration<String> aliases = PKCS12KeyStoreUtils.loadAliases(
+     *          inputStream,
+     *          "000000"
+     *          );
+     * }</pre>
+     *
+     * @param inputStream keyStore输入流, 完成后会关闭
+     * @param keystorePassword keyStore密码
+     */
+    public static Enumeration<String> loadAliasesAdvanced(InputStream inputStream, String keystorePassword) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException, NoSuchProviderException {
+        try {
+            KeyStore keyStore = KeyStore.getInstance(ALGORITHM, "BC");
+            keyStore.load(inputStream, keystorePassword != null ? keystorePassword.toCharArray() : null);
+            return keyStore.aliases();
+        } finally {
+            CloseableUtils.closeQuiet(inputStream);
+        }
+    }
+
+    /**
+     * 从p12/pfx文件中读取证书和私钥, BouncyCastle实现, 支持SM2
+     *
+     * <pre>{@code
+     *      PKCS12KeyStoreUtils.CertificateChainAndKey certificateChainAndKey = PKCS12KeyStoreUtils.loadCertificateAndKey(
+     *          "ca-cert.p12",
+     *          "000000",
+     *          "Thistle test ca alias"
+     *          );
+     * }</pre>
+     *
+     * @param keyStorePath keyStore路径
+     * @param keystorePassword keyStore密码
+     * @param alias 证书和私钥的别名
+     */
+    public static CertificateChainAndKey loadCertificateAndKeyAdvanced(String keyStorePath, String keystorePassword, String alias) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException, NoSuchProviderException {
+        File keyStoreFile = new File(keyStorePath);
+        if (!keyStoreFile.exists()){
+            throw new IOException("Can not find keyStore file, path:" + keyStoreFile.getAbsolutePath());
+        }
+        return loadCertificateAndKeyAdvanced(new FileInputStream(keyStoreFile), keystorePassword, alias);
+    }
+
+    /**
+     * 从p12/pfx文件中读取证书和私钥, BouncyCastle实现, 支持SM2
+     *
+     * <pre>{@code
+     *      PKCS12KeyStoreUtils.CertificateChainAndKey certificateChainAndKey = PKCS12KeyStoreUtils.loadCertificateAndKey(
+     *          inputStream,
+     *          "000000",
+     *          "Thistle test ca alias"
+     *          );
+     * }</pre>
+     *
+     * @param inputStream keyStore输入流, 完成后会关闭
+     * @param keystorePassword keyStore密码
+     * @param alias 证书和私钥的别名
+     */
+    public static CertificateChainAndKey loadCertificateAndKeyAdvanced(InputStream inputStream, String keystorePassword, String alias) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException, NoSuchProviderException {
+        try {
+            KeyStore keyStore = KeyStore.getInstance(ALGORITHM, "BC");
+            keyStore.load(inputStream, keystorePassword != null ? keystorePassword.toCharArray() : null);
+            java.security.cert.Certificate[] certificateChain = keyStore.getCertificateChain(alias);
+            if (certificateChain == null){
+                java.security.cert.Certificate certificate = keyStore.getCertificate(alias);
+                if (certificate != null) {
+                    certificateChain = new java.security.cert.Certificate[]{certificate};
+                }
+            }
+            return new CertificateChainAndKey(alias, certificateChain, (PrivateKey) keyStore.getKey(alias, keystorePassword != null ? keystorePassword.toCharArray() : null));
+        } finally {
+            CloseableUtils.closeQuiet(inputStream);
+        }
+    }
+
+    /**
+     * 从p12/pfx文件中读取证书和私钥, BouncyCastle实现, 支持SM2
+     *
+     * <pre>{@code
+     *      List<PKCS12KeyStoreUtils.CertificateChainAndKey> certificateChainAndKeyList = PKCS12KeyStoreUtils.loadAllCertificateAndKey(
+     *          "ca-cert.p12",
+     *          "000000"
+     *          );
+     * }</pre>
+     *
+     * @param keyStorePath keyStore路径
+     * @param keystorePassword keyStore密码
+     */
+    public static List<CertificateChainAndKey> loadAllCertificateAndKeyAdvanced(String keyStorePath, String keystorePassword) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException, NoSuchProviderException {
+        File keyStoreFile = new File(keyStorePath);
+        if (!keyStoreFile.exists()){
+            throw new IOException("Can not find keyStore file, path:" + keyStoreFile.getAbsolutePath());
+        }
+        return loadAllCertificateAndKeyAdvanced(new FileInputStream(keyStoreFile), keystorePassword);
+    }
+
+    /**
+     * 从p12/pfx文件中读取证书和私钥, BouncyCastle实现, 支持SM2
+     *
+     * <pre>{@code
+     *      List<PKCS12KeyStoreUtils.CertificateChainAndKey> certificateChainAndKeyList = PKCS12KeyStoreUtils.loadAllCertificateAndKey(
+     *          inputStream,
+     *          "000000"
+     *          );
+     * }</pre>
+     *
+     * @param inputStream keyStore输入流, 完成后会关闭
+     * @param keystorePassword keyStore密码
+     */
+    public static List<CertificateChainAndKey> loadAllCertificateAndKeyAdvanced(InputStream inputStream, String keystorePassword) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException, NoSuchProviderException {
+        try {
+            KeyStore keyStore = KeyStore.getInstance(ALGORITHM, "BC");
+            keyStore.load(inputStream, keystorePassword != null ? keystorePassword.toCharArray() : null);
+            List<CertificateChainAndKey> list = new ArrayList<>(1);
+            Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                String alias = aliases.nextElement();
+                java.security.cert.Certificate[] certificateChain = keyStore.getCertificateChain(alias);
+                if (certificateChain == null) {
+                    java.security.cert.Certificate certificate = keyStore.getCertificate(alias);
+                    if (certificate != null) {
+                        certificateChain = new Certificate[]{certificate};
+                    }
+                }
+                list.add(new CertificateChainAndKey(alias, certificateChain, (PrivateKey) keyStore.getKey(alias, keystorePassword != null ? keystorePassword.toCharArray() : null)));
+            }
+            return list;
+        } finally {
+            CloseableUtils.closeQuiet(inputStream);
+        }
     }
 
 }
