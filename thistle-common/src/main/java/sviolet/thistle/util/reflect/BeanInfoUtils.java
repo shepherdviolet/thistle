73a8995c4a6a21bce482a135dfad791df8b75efe
shepherdviolet <shepherdviolet@163.com>
2020-02-16 21:56:35 +0800

    1> New BeanInfoUtils

---
+++ thistle-common/src/main/java/sviolet/thistle/util/reflect/BeanInfoUtils.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (C) 2015-2020 S.Violet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project GitHub: https://github.com/shepherdviolet/thistle
+ * Email: shepherdviolet@163.com
+ */
+
+package sviolet.thistle.util.reflect;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Bean info utils
+ *
+ * @author S.Violet
+ */
+public class BeanInfoUtils {
+
+    /**
+     * Get property info of Java Bean, No cache
+     *
+     * @param beanClass Bean class
+     * @throws IntrospectionException Inspect error
+     */
+    public static Map<String, PropertyInfo> getPropertyInfos(Class<?> beanClass) throws IntrospectionException {
+        if (beanClass == null) {
+            throw new NullPointerException("beanClass is null");
+        }
+
+        BeanInfo beanInfo = Introspector.getBeanInfo(beanClass, Object.class);
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        Map<String, PropertyInfo> propertyInfos = new HashMap<>(propertyDescriptors.length << 1);
+        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+            Class<?> propertyClass = propertyDescriptor.getPropertyType();
+            Type propertyType;
+            Method readMethod = propertyDescriptor.getReadMethod();
+            Method writeMethod = propertyDescriptor.getWriteMethod();
+            Class<?> declaringClass;
+            if (propertyClass == null) {
+                propertyClass = Object.class;
+            }
+            if (readMethod != null) {
+                propertyType = readMethod.getGenericReturnType();
+                declaringClass = readMethod.getDeclaringClass();
+            } else {
+                // writeMethod must exist if readMethod is null
+                Type[] parameterTypes = writeMethod.getGenericParameterTypes();
+                // IndexedPropertyDescriptor has 2 parameters, see Introspector
+                propertyType = parameterTypes[parameterTypes.length - 1];
+                declaringClass = writeMethod.getDeclaringClass();
+            }
+            if (propertyType instanceof TypeVariable && ((TypeVariable<?>) propertyType).getGenericDeclaration() instanceof Class) {
+                Type actualType = null;
+                try {
+                    // try to find generic type in class
+                    actualType = GenericClassUtils.getActualTypes(beanClass, declaringClass)
+                            .get(((TypeVariable<?>) propertyType).getName());
+                } catch (GenericClassUtils.TargetGenericClassNotFoundException ignore) {
+                }
+                if (!isAcceptedType(actualType)) {
+                    actualType = propertyClass;
+                }
+                propertyType = actualType;
+            } else if (!isAcceptedType(propertyType)) {
+                propertyType = propertyClass;
+            }
+            propertyInfos.put(propertyDescriptor.getName(), new PropertyInfo(
+                    propertyDescriptor.getName(),
+                    propertyClass,
+                    propertyType,
+                    readMethod,
+                    writeMethod
+            ));
+        }
+        return propertyInfos;
+    }
+
+    private static boolean isAcceptedType(Type type) {
+        return type instanceof Class || type instanceof ParameterizedType;
+    }
+
+    public static class PropertyInfo {
+
+        private String propertyName;
+        private Class<?> propertyClass;
+        private Type propertyType;
+        private Method readMethod;
+        private Method writeMethod;
+
+        private PropertyInfo(String propertyName, Class<?> propertyClass, Type propertyType, Method readMethod, Method writeMethod) {
+            this.propertyName = propertyName;
+            this.propertyClass = propertyClass;
+            this.propertyType = propertyType;
+            this.readMethod = readMethod;
+            this.writeMethod = writeMethod;
+        }
+
+        /**
+         * Bean property name, Not null
+         */
+        public String getPropertyName() {
+            return propertyName;
+        }
+
+        /**
+         * Class of property, Not null
+         */
+        public Class<?> getPropertyClass() {
+            return propertyClass;
+        }
+
+        /**
+         * Generic type of property, Not null
+         */
+        public Type getPropertyType() {
+            return propertyType;
+        }
+
+        /**
+         * Read method, Nullable, But one of the two methods must not be null
+         */
+        public Method getReadMethod() {
+            return readMethod;
+        }
+
+        /**
+         * Write method, Nullable, But one of the two methods must not be null
+         */
+        public Method getWriteMethod() {
+            return writeMethod;
+        }
+
+        @Override
+        public String toString() {
+            return "PropertyInfo{" +
+                    "propertyName='" + propertyName + '\'' +
+                    ", propertyClass=" + propertyClass +
+                    ", propertyType=" + propertyType +
+                    ", readMethod=" + readMethod +
+                    ", writeMethod=" + writeMethod +
+                    '}';
+        }
+    }
+
+}
---
+++ thistle-common/src/test/java/sviolet/thistle/util/reflect/BeanInfoUtilsTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2015-2020 S.Violet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project GitHub: https://github.com/shepherdviolet/thistle
+ * Email: shepherdviolet@163.com
+ */
+
+package sviolet.thistle.util.reflect;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.beans.IntrospectionException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class BeanInfoUtilsTest {
+
+    @Test
+    public void getPropertyInfos() throws IntrospectionException {
+        Map<String, BeanInfoUtils.PropertyInfo> propertyInfos = new TreeMap<>(BeanInfoUtils.getPropertyInfos(Bean2.class));
+        StringBuilder stringBuilder = new StringBuilder();
+        for (BeanInfoUtils.PropertyInfo propertyInfo : propertyInfos.values()) {
+            stringBuilder.append(propertyInfo).append("\n");
+        }
+        Assert.assertEquals("PropertyInfo{propertyName='a', propertyClass=interface java.util.Map, propertyType=java.util.Map<java.lang.String, java.lang.Object>, readMethod=public java.util.Map sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean1.getA(), writeMethod=public void sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean0.setA(java.lang.Object)}\n" +
+                "PropertyInfo{propertyName='b', propertyClass=class java.lang.Long, propertyType=class java.lang.Long, readMethod=public java.lang.Object sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean0.getB(), writeMethod=public void sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean1.setB(java.lang.Long)}\n" +
+                "PropertyInfo{propertyName='c', propertyClass=interface java.util.Map, propertyType=java.util.Map<java.lang.String, java.util.List<java.lang.String>>, readMethod=null, writeMethod=public void sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean0.setC(java.lang.Object)}\n" +
+                "PropertyInfo{propertyName='d', propertyClass=interface java.util.Set, propertyType=java.util.Set<java.lang.Runnable>, readMethod=null, writeMethod=public void sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean0.setD(java.lang.Object)}\n" +
+                "PropertyInfo{propertyName='e', propertyClass=class java.lang.Object, propertyType=class java.lang.Object, readMethod=public java.lang.Object sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean0.getE(), writeMethod=null}\n" +
+                "PropertyInfo{propertyName='int', propertyClass=int, propertyType=int, readMethod=public int sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean1.getInt(), writeMethod=null}\n" +
+                "PropertyInfo{propertyName='list', propertyClass=interface java.util.List, propertyType=java.util.List<java.lang.String>, readMethod=public java.util.List sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean0.getList(), writeMethod=public void sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean0.setList(java.util.List)}\n" +
+                "PropertyInfo{propertyName='string', propertyClass=class java.lang.String, propertyType=class java.lang.String, readMethod=public java.lang.String sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean0.getString(), writeMethod=public void sviolet.thistle.util.reflect.BeanInfoUtilsTest$Bean0.setString(java.lang.String)}\n",
+                stringBuilder.toString());
+//        System.out.println(stringBuilder.toString());
+    }
+
+    public static class Bean0 <A, B, C, D, E> {
+
+        public A getA(){
+            return null;
+        }
+
+        public void setA(A i) {
+        }
+
+        public B getB(){
+            return null;
+        }
+
+        public void setC(C i) {
+        }
+
+        public void setD(D i) {
+        }
+
+        public E getE(){
+            return null;
+        }
+
+        public String getString(){
+            return null;
+        }
+
+        public void setString(String i) {
+        }
+
+        public List<String> getList(){
+            return null;
+        }
+
+        public void setList(List<String> i) {
+        }
+
+        public void setInt(long i) {
+        }
+
+    }
+
+    public static class Bean1 <AA, BB> extends Bean0<Map<String, Object>, Long, Map<String, List<String>>, AA, BB> {
+
+        @Override
+        public Map<String, Object> getA() {
+            return null;
+        }
+
+        public void setB(Long i){
+        }
+
+        public void setString(byte[] i) {
+        }
+
+        public int getInt(){
+            return 0;
+        }
+
+    }
+
+    public static class Bean2 <AAA> extends Bean1<Set<Runnable>, AAA> {
+
+    }
+
+}
